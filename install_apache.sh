8cf022095f5d8f3a249259c103317b498ee031bd
mehoussou <115427870+mehoussou@users.noreply.github.com>
2023-01-31 00:25:44 -0700

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+# #!/bin/bash
 
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "WELCOME TO INFOTECH from $(hostname -f)" > /var/www/html/index.html
+# yum update -y
+# yum install -y httpd
+# systemctl start httpd
+# systemctl enable httpd
+# echo "WELCOME TO INFOTECH from $(hostname -f)" > /var/www/html/index.html
+
+ #!/bin/bash
+ sudo yum update -y && sudo yum install -y docker
+ sudo systemctl start docker
+ sudo usermod -aG docker ec2-user
+ docker run -d -p 8080:80 nginx
